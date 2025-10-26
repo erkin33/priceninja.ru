71f1f211d203fd056a0ebd8c0507f301a3692098
Erkin <erkinbojurmanov3@gmail.com>
2025-10-26 23:23:35 +0500

    added-page

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="cbJ17nOtbRhYABH7nl0NBNpNl8RbHdBtmG7C-YFPjVw"
+      />
+      <meta name="yandex-verification" content="b093a6c3558efc9e" />
+      <title>Price Ninja</title>
+      <meta name="description" content="PriceNinja — анализ цен и товаров." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
